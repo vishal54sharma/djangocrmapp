71635d5bcd234327af3e2d096236c24f42d330a1
vishal54sharma <vishal54sharma@gmail.com>
2023-08-30 10:25:53 +0530

    updating more features

--- base/views.py
+++ base/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from django.http import HttpRequest
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def home(request):
+    if request.method == 'POST':
+       username = request.POST['username'] 
+       password = request.POST['password']
+       user = authenticate(request,username=username,password=password)
+       if user is not  None:
+           login(request, user)
+           messages.success(request,"You have been logged in")
+           return redirect('home')
+       else:
+           messages.success(request,"Oops there was an error. Please try again...")
+           return redirect('home')
+           
+    else:
+        return render(request,'home.html',{})
+
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"You ve been logged out...")
+    return redirect('home')
+
+def register_user(request):
+    # form  = UserCreationForm()
+    return render(request,'register.html',{})
+
+
+
